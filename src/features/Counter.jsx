fb99a809e0e31c8b74805c0a4af15a009c5c7bd9
martapla <martaplanella@hotmail.com>
2024-03-20 19:59:57 +0100

    first commit

---
+++ src/features/Counter.jsx
@@ -0,0 +1,43 @@
+import { useSelector, useDispatch } from "react-redux"
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice"
+import {useState} from "react"
+
+const Counter = () => {
+    const count = useSelector((state) => state.counter.count);
+    const dispatch = useDispatch()
+
+    const [incrementAmount, setIncrementAmount] = useState(0);
+
+    const addValue = Number(incrementAmount) || 0;
+
+    const resetAll = () => {
+        setIncrementAmount(0);
+        dispatch(reset());
+    }
+
+
+  return (
+     
+      <section className="counter">
+            <p>{count}</p>
+                <div>
+                    <button
+                        className="btn"
+                        onClick={() => dispatch(increment())}>+</button>
+                    
+                    <button
+                        className="btn"
+                        onClick={() => dispatch(decrement())}>-</button>
+                </div>
+          
+                <input
+                    type="text"
+                    value={incrementAmount}
+                    onChange={(e) => setIncrementAmount(e.target.value)}
+                />
+            
+      </section>
+  )
+}
+
+export default Counter
